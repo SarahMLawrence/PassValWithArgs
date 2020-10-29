ce700bfa5b006151fb377726ef7d4d70bb2d3c2b
Sarah Lawrence <sarahlawrence1152@gmail.com>
2020-10-29 19:49:27 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/*Create a function called functionWithArgs that accepts two arguments and outputs their sum to the dev console.
+Call the function with two numbers as arguments. */
+
+function functionWithArgs(num1, num2) {
+  console.log(num1 + num2)
+
+}
+
+functionWithArgs(7, 9)
